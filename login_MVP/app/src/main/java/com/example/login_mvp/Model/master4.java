a66167f1d5661835f2217185c7e32bc4b5652133
pengzhaoyang <1058032729@qq.com>
2018-01-30 20:53:26 +0800

    master-branch-add-master4

--- login_MVP/app/src/main/java/com/example/login_mvp/Model/master4.java
+++ login_MVP/app/src/main/java/com/example/login_mvp/Model/master4.java
@@ -6,4 +6,13 @@ package com.example.login_mvp.Model;
  * comment :
  */
 public class master4 {
+    public String getMaster4() {
+        return master4;
+    }
+
+    public void setMaster4(String master4) {
+        this.master4 = master4;
+    }
+
+    String master4;
 }
