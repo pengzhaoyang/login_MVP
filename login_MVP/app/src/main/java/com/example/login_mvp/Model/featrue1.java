133770d7f3b684e531f5fd6bf512425f61cb2e0c
pengzhaoyang <1058032729@qq.com>
2018-01-30 18:11:59 +0800

    commit-pzy

---
+++ login_MVP/app/src/main/java/com/example/login_mvp/Model/featrue1.java
@@ -0,0 +1,38 @@
+package com.example.login_mvp.Model;
+
+/**
+ * author  :  pengzhaoyang
+ * date    :  2018/1/30.
+ * comment :
+ */
+public class featrue1 {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFeature_model() {
+        return feature_model;
+    }
+
+    public void setFeature_model(String feature_model) {
+        this.feature_model = feature_model;
+    }
+
+    String feature_model;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    int num;
+}
